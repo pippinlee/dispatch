194822ac8bc92f8502a3058df3c6725c7432dc2a
Ben Cook <b@bencook.ca>
2015-01-15 21:34:52 -0800

    Adding list_display for Article, Image, Video

--- dispatch/apps/content/admin.py
+++ dispatch/apps/content/admin.py
@@ -12,6 +12,7 @@ class ArticleAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("short_headline",)}
 
     form = ArticleForm
+    list_display = ('long_headline', 'updated_at')
 
     fieldsets = (
         ('Content', {
@@ -31,9 +32,15 @@ class ArticleAdmin(admin.ModelAdmin):
         }),
     )
 
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ('img', 'updated_at')
+
+class VideoAdmin(admin.ModelAdmin):
+    list_display = ('title', 'url', 'updated_at')
+
 admin.site.register(Article, ArticleAdmin)
 admin.site.register(Section)
-admin.site.register(Image)
-admin.site.register(Video)
+admin.site.register(Image, ImageAdmin)
+admin.site.register(Video, VideoAdmin)
 admin.site.register(Tag)
 admin.site.register(Topic)
